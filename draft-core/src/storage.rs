18b76f31a41211f709c27230befecbd2dda6b925
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2022-09-18 18:21:55 -0500

    Remove some unused PartialEq impl for BufferBackend. Improve coverage for storage.rs
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- draft-core/src/storage.rs
+++ draft-core/src/storage.rs
@@ -36,46 +36,6 @@ pub struct BufferBackend {
     _inner: Arc<Mutex<Vec<u8>>>,
 }
 
-impl PartialEq for BufferBackend {
-    /// Determine whether two BufferBackends are equal.
-    ///
-    /// It is important to first compare the _inner buffer
-    /// by reference. Since it is behind an Arc<Mutex<...>>,
-    /// if we try to lock the same memory, we deadlock. Only if
-    /// the references point to different memory locations, should
-    /// we try to lock and compare the inner value.
-    fn eq(&self, other: &Self) -> bool {
-        // Check by reference first.
-        let ptr_equality = Arc::ptr_eq(&self._inner, &other._inner);
-
-        // Fall back to the locking the mutexes and comparing the inner buffer.
-        ptr_equality
-            || *self
-                ._inner
-                .lock()
-                .expect("Failed to lock the internal buffer.")
-                == *other
-                    ._inner
-                    .lock()
-                    .expect("Failed to lock internal buffer.")
-    }
-}
-
-impl Eq for BufferBackend {}
-
-impl BufferBackend {
-    pub fn with_capacity(capacity: usize) -> Self {
-        Self {
-            _inner: Arc::new(Mutex::new(Vec::with_capacity(capacity))),
-        }
-    }
-    pub fn new() -> Self {
-        Self {
-            _inner: Arc::new(Mutex::new(Vec::with_capacity(1024))),
-        }
-    }
-}
-
 impl Default for BufferBackend {
     fn default() -> Self {
         Self {
