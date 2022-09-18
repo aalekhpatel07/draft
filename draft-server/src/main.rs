3777480d76d91a5463f4179c269771d8106844aa
Mark Johnston <git@mhj.ca>
2022-09-17 22:56:21 -0500

    Tracing & OpenTelemetry

--- draft-server/src/main.rs
+++ draft-server/src/main.rs
@@ -6,8 +6,11 @@ use draft_core::{
 };
 use draft_server::RaftRuntime;
 use tokio::net::UdpSocket;
-use tracing::Level;
+use tracing::{info_span, Instrument, Level};
 use structopt::StructOpt;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[derive(StructOpt)]
 #[structopt(name="Raft Server.", about = "The runtime for Raft. (without the state-machine backend for now).")]
@@ -33,12 +36,31 @@ pub async fn main() -> color_eyre::Result<()> {
         }
     };
 
-    tracing_subscriber::fmt()
-    .with_max_level(log_level)
-    .init();
-    
+    // Set up the Jaeger sender
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name("draft")
+        .install_batch(opentelemetry::runtime::Tokio)?;
+
+    // The OpenTelemetry layer that listens for tracing calls and turns them into OpenTelemetry requests
+    let otel_layer = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    // The layer that dumps logs to the console
+    let console_layer = tracing_subscriber::fmt::layer().pretty();
+
+    // The tracing subscriber that arranges for our traces to go through the layers we want
+    let subscriber = Registry::default()
+        .with(otel_layer)
+        .with(console_layer);
+
+    // Install the subscriber as the global default
+    subscriber.init();
+
     let config = load_from_file(opt.config)?;
     let raft: RaftRuntime<BufferBackend, UdpSocket> = RaftRuntime::new(config);
-    raft.run().await?;
+    raft.run().instrument(info_span!("app")).await?;
+
+    // Cleanly shut down opentelemetry so all queued requests can be sent out
+    opentelemetry::global::shutdown_tracer_provider();
+
     Ok(())
 }
